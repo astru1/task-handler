09d5a274ced2be51d6e1a1246cd0be2eab78bfd3
astru2 <mishapon@mail.ru>
2022-08-02 17:09:54 +0300

    fix

--- rabbit/publisher.go
+++ rabbit/publisher.go
@@ -39,61 +39,6 @@ func InitQueue(connectionString, queueName string) (*LocalQueue, error) {
 		ch:        ch,
 	}, nil
 }
-
-//
-//func main() {
-//	fmt.Println("Starting up")
-//	conn, err := amqp.Dial("amqp://guest@localhost:5672/")
-//	if err != nil {
-//		fmt.Println(err)
-//		panic(err)
-//	}
-//	defer conn.Close()
-//
-//	fmt.Println("Successfully connected to RabbitMQ Instance")
-//
-//	ch, err := conn.Channel()
-//	if err != nil {
-//		fmt.Println(err)
-//		panic(err)
-//	}
-//	defer ch.Close()
-//
-//	q, err := ch.QueueDeclare(
-//		"TestQueue",
-//		false,
-//		false,
-//		false,
-//		false,
-//		nil,
-//	)
-//
-//	if err != nil {
-//		fmt.Println(err)
-//		panic(err)
-//	}
-//
-//	fmt.Println(q)
-//
-//	err = ch.Publish(
-//		"",
-//		"TestQueue",
-//		false,
-//		false,
-//		amqp.Publishing{
-//			ContentType: "text/Plain",
-//			Body:        []byte("Hello World"),
-//		},
-//	)
-//
-//	if err != nil {
-//		fmt.Println(err)
-//		panic(err)
-//	}
-//
-//	fmt.Println("Successfully published message to queue")
-//
-//}
 func (lq *LocalQueue) AddToQueue(task database.Task) error {
 	marshaledTask, err := json.Marshal(task)
 	if err != nil {
